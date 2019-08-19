825da94148941be3c1de68666580f76d1efc4e7b
Allen Li <allenlee1995@hotmail.com>
2019-08-18 21:32:19 -0600

    setting up opencv2 for QT creator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
